04f8fbd625ec71e78c1ac56387c4ced48108b41b
Henry <hnr.leto11@gmail.com>
2020-11-09 12:45:47 -0500

    kitten api fun

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,37 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
+  end
 
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+  end
+
+  private
+
+  def kitten_params
+    require(:kitten).permit(:name, :age, :softness, :cuteness)
   end
 end
